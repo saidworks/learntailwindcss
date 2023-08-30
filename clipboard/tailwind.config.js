2701560a07cbd3c9febb16d818c2c188ead59ba2
Said Zitouni <zitouni.sd@gmail.com>
2023-08-29 20:09:39 -0400

    remove practice folder and add last modification to clipboard

---
+++ clipboard/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    extend: {
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        Tilt: [`Tilt Prism`, `sans-serif`],
+        Lora: [`Lora`, `serif`],
+        LoraRegular: [`LoraRegular`, `serif`],
+      },
+    },
+  },
+  plugins: [],
+};
